e8300d12862641f72b93930cfa640a9b057cc82f
federico Tartarini <federicotartarini@gmail.com>
2024-07-30 12:30:28 +1000

    feat: changed website card

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -76,7 +76,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: "img/docusaurus-social-card.jpg",
+      image: "img/website-card.png",
       navbar: {
         title: "My Site",
         logo: {
